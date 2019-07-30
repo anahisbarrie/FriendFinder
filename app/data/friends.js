ca507e56f285d306d096acf6b9c210274cd1a531
anahisbarrie <anahis.barrie@gmail.com>
2019-07-29 20:43:13 -0400

    adding heroku link

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,12 +1,12 @@
 
 var friends = [
     {
-        name: "Charlie",
+        name: "Carlitos",
         photo: "https://vignette3.wikia.nocookie.net/itsalwayssunny/images/0/0a/Charlie_%289%29.jpg",
         scores: [5, 1, 2, 3, 1, 2, 5, 1, 1, 1]
     },
     {
-        name: "Sweet Dee",
+        name: "Catira",
         photo: "https://vignette2.wikia.nocookie.net/itsalwayssunny/images/f/f3/Square-Dee-Cheesy.jpg",
         scores: [2, 5, 4, 1, 2, 3, 3, 2, 3, 3]
     },
